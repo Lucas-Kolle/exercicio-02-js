3f896ffd44317fa1b1f6ff2b253a166c6ed4c0a4
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-02-06 10:05:33 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -49,6 +49,106 @@ entradaDeDados.question("Digite o nome do cliente:", function (nome){
                     
                     //criando variável para trabalhar com o tempo
                     let tempoPagamento = tempo
+
+                    //criando variável para guardar o montante final
+                    let montante
+                    
+                    //criando condicional para decidir o tempo em anos ou meses
+                    if(tempo == 2){
+                        
+                        //coletando o tempo em anos
+                        entradaDeDados.question("Digite o tempo em ANOS:", function(tempoA){
+
+                            //criando variável para trabalhar com o tempo em anos
+                            let tempoEmAnos = tempoA
+
+                            //convertendo anos em meses
+                            let tempoConvertido
+                            tempoConvertido = Number(tempoEmAnos) * 12
+
+                            /*criando validações*/
+                            //validando campos vazios
+                            if(nomeCliente == "" || nomeProduto == "" || valorCompra == "" || taxaJuros == "" || tempoPagamento == "" || tempoEmAnos == ""){
+                                console.log("ERRO: Todos os campos devem ser preenchidos!!")
+
+                            //validando números nos nomes (cliente e produto)
+                            //colocar a " ! " antes do isNaN inverte a saída (se for um número vai sair verdadeiro)
+                            }else if(!isNaN(nomeCliente) || !isNaN(nomeProduto) ){ // não precisa colocar == true, pois o else e if já trabalha com boolean
+                                console.log("ERRO: Não é permitido a entrada de números nos nomes!!")
+
+                            //validando letras nos campos númericos    
+                            }else if(isNaN(valorCompra) || isNaN(taxaJuros) || isNaN(tempoPagamento) || isNaN(tempoEmAnos)){
+                                console.log("ERRO: Não é permitido a entrada de letras nos campos númericos")
+
+                            //bloquando a entrada de números negativos (menores que zero)
+                            }else if(valorCompra < 0 || taxaJuros < 0 || tempoPagamento < 0 || tempoEmAnos < 0){
+                                console.log("ERRO: Não é permitido a entrada de números negativos!!!")
+
+                            //continuando projeto
+                            }else{
+                                
+                                //calculando montante final
+                                montante = Number(valorCompra) * (1 + Number(taxaConvertida)) ** tempoConvertido
+
+                                //calculando a diferença do montando em relação ao valor da compra
+                                let diferencaDoValor
+                                diferencaDoValor = Number(montante) - Number(valorCompra)
+
+                                //realizando saída
+                                console.log("")
+                                console.log("******************* VIVA MODA *******************")
+                                console.log(`Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\nA compra do produto ${nomeProduto}, tem um valor de: ${valorCompra}.\nA sua compra será parcelada em ${tempoConvertido} vezes e o Sr(a) pagará: ${montante.toFixed(2)}.\nO acréscimo realizado ao valor de: ${valorCompra} será de ${diferencaDoValor.toFixed(2)}.`)
+                                console.log(`Muito obrigado por escolher a VIVA MODAS.\n*******************************************************`)
+                            }
+
+                        })//fecha tempoA
+                    
+                    //tempo em meses
+                    }else{
+
+                        //coletando o tempo em meses
+                        entradaDeDados.question("Digite o tempo em MESES:", function(tempoM){
+
+                            //criando variável para trabalhar com o tempo em meses
+                            let tempoEmMeses = tempoM
+
+                            if(tempoPagamento > 2){
+                                console.log("ERRO: Você deve digitar a opção conforme orientado!!")
+
+                            }else if(nomeCliente == "" || nomeProduto == "" || valorCompra == "" || taxaJuros == "" || tempoPagamento == "" || tempoEmMeses == "" ){
+                                console.log("ERRO: Todos os campos devem ser preenchidos!!")
+
+                            
+                            }else if(!isNaN(nomeCliente) || !isNaN(nomeProduto) ){ // não precisa colocar == true, pois o else e if já trabalha com boolean
+                                console.log("ERRO: Não é permitido a entrada de números nos nomes!!")
+
+                            }else if(isNaN(valorCompra) || isNaN(taxaJuros) || isNaN(tempoPagamento) || isNaN(tempoEmMeses)){
+                                console.log("ERRO: Não é permitido a entrada de letras nos campos númericos")
+
+                            }else if(valorCompra < 0 || taxaJuros < 0 || tempoPagamento < 0 || tempoEmMeses < 0){
+                                console.log("ERRO: Não é permitido a entrada de números negativos!!!")
+
+                            }else if(valorCompra < 0 || taxaJuros < 0 || tempoPagamento < 0 || tempoEmMeses < 0){
+                                console.log("ERRO: Não é permitido a entrada de números negativos!!!")
+
+                            }else{
+                                
+                                //calculando montante final
+                                montante = Number(valorCompra) * (1 + Number(taxaConvertida)) ** tempoEmMeses
+
+                                //calculando a diferença do montando em relação ao valor da compra
+                                let diferencaDoValor
+                                diferencaDoValor = Number(montante) - Number(valorCompra)
+
+                                //realizando saída
+                                console.log("")
+                                console.log("******************* VIVA MODA *******************")
+                                console.log(`Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\nA compra do produto ${nomeProduto}, tem um valor de: ${valorCompra}.\nA sua compra será parcelada em ${tempoEmMeses} vezes e o Sr(a) pagará: ${montante.toFixed(2)}.\nO acréscimo realizado ao valor de: ${valorCompra} será de ${diferencaDoValor.toFixed(2)}.`)
+                                console.log(`Muito obrigado por escolher a VIVA MODAS.\n*******************************************************`)
+                            }
+
+                        })//fecha tempoM
+                    }
                 })//fecha tempo
             })//fecha juros
         })//fecha valor
